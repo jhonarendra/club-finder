c3645dac249908cef3d6f47094d0c0908d467bc0
jhonarendra <jhonarendra@gmail.com>
2021-03-30 10:22:13 +0800

    async

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -3,9 +3,23 @@ const main = () => {
     const buttonSearchElement = document.querySelector("#searchButtonElement");
     const clubListElement = document.querySelector("#clubList");
 
-    const onButtonSearchClicked = () => {
-        let dataSource = new DataSource(renderResult, fallbackResult);
-        dataSource.searchClub(searchElement.value);
+    const onButtonSearchClicked = async() => { // kalau async await pake 'async', kalau .then() .catch() gak perlu
+        // let dataSource = new DataSource(renderResult, fallbackResult);
+        // dataSource.searchClub(searchElement.value);
+
+        // bisa ini
+        // DataSource.searchClub(searchElement.value)
+        // .then(renderResult)
+        // .catch(fallbackResult)
+
+        // atau async await
+        try {
+            const result = await DataSource.searchClub(searchElement.value);
+            renderResult(result);
+        } catch(message){
+            fallbackResult(message)
+        }
+
     };
 
     const renderResult = results => {
