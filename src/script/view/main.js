f7fbc219bcdf03596fa7ea46e2642ed798bfb2f1
jhonarendra <jhonarendra@gmail.com>
2021-03-30 15:02:43 +0800

    search-bar

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,8 +1,7 @@
 import DataSource from '../data/data-source.js';
 
 const main = () => {
-    const searchElement = document.querySelector("#searchElement");
-    const buttonSearchElement = document.querySelector("#searchButtonElement");
+    const searchElement = document.querySelector("search-bar");
     const clubListElement = document.querySelector("#clubList");
 
     const onButtonSearchClicked = async() => { // kalau async await pake 'async', kalau .then() .catch() gak perlu
@@ -53,6 +52,6 @@ const main = () => {
         clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`
     };
 
-    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+    searchElement.clickEvent = onButtonSearchClicked;
 };
 export default main;
